f0553e4bc1c47182143187c9831134e7dbf86bdc
bastien dubray <bastien.dubray@gmail.com>
2024-02-26 16:51:00 +0100

    Add seed not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Friend.create(first_name: "Camille", location: "Bordeaux", content:"J'aime la littératures", gender: "Womem", available: true, age: 25, good_at: "Raconter des histoires", price: 40)
+Friend.create(first_name: "Bastien", location: "Bayonne", content:"Je surf ", available: true, gender: 'Man', age: 25, good_at: "Faire de la musique", price: 40)
+Friend.create(first_name: "Thomas", location: "Bordeaux", content:"J'aime faire du sport", available: true, gender: 'Man', age: 40, good_at: "Faire du sport", price: 40)
+Friend.create(first_name: "Jean-paul", location: "Paris", content:"J'aime les jeux de sociétés", available: true, gender: 'Men', age: 28, good_at: "Aller au bar", price: 40)
