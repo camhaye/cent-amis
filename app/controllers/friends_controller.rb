5763ef05147d0d605b1857dc2679c56679c398a8
Camille Haye <ca.haye@outlook.fr>
2024-02-26 16:11:18 +0100

    controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,31 @@
 class FriendsController < ApplicationController
+  before_action :set_friend, only: [:show, :edit, :destroy]
 
   def index
     @friends = Friend.all
+  end
+
+  def show
+  end
+
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    @friend = Friend.new(params_friend)
+    if @friend.save
+      redirect_to friend_path(@friend)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+
+  def set_friend
+    @friend = Friend.find(params[:id])
+  end
+
+  def params_friend
+    params.require(:friend).permit(:name, )
   end
 end
