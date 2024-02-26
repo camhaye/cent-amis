4b71795d4f00ee2f72c928b1e0d022f6f8c3196a
Thomas Maumet <thomasmaumet@free.fr>
2024-02-26 17:02:21 +0100

    Setup dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,20 @@
 class DashboardController < ApplicationController
-  
+
+  def my_friends
+    @friend = Friend.where(user_id: params_id)
+  end
+
+  def my_bookings
+
+  end
+
+  def validate
+    # ajouter booléen
+
+  end
+
+  def my_booked_friends
+
+  end
+
 end
