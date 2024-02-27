34d7c822962407614f44df00875e4954908a36bb
bastien dubray <bastien.dubray@gmail.com>
2024-02-27 14:33:51 +0100

    Finish method new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_friend, only: %i[new create]
+  before_action :set_user, only: %i[new create]
 
   def new
     @friend = Friend.find(params[:friend_id])
@@ -9,19 +10,23 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @user = current_user.id
-    @booking.user_id = @user
+    @booking.user_id = @user.id
     @booking.friend_id = @friend.id
-
+    @booking.total = @booking.total_price(@booking.start_date, @booking.end_date, @friend.price)
+    @friend.available = false
     if @booking.save
-      redirect_to friend_path(@friend)
+      redirect_to friends_path()
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  private
   
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
   def set_friend
     @friend = Friend.find(params[:friend_id])
   end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :friend
+
+  def total_price(start_date, end_date, price)
+    number_of_days = (end_date - start_date).to_i + 1
+    return number_of_days * price
+  end
 end
