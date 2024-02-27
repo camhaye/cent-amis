d1765533d5587d2266b90879946913106784becd
bastien dubray <bastien.dubray@gmail.com>
2024-02-27 12:05:56 +0100

    Add new & Create booking + View

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,32 @@
 class BookingsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :set_friend, only: %i[new create]
+
   def new
+    @friend = Friend.find(params[:friend_id])
     @booking = Booking.new
   end
 
   def create
-    raise
     @booking = Booking.new(booking_params)
-    @booking.save
+    @user = current_user.id
+    @booking.user_id = @user
+    @booking.friend_id = @friend.id
+
+    if @booking.save
+      redirect_to friend_path(@friend)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def set_user
-    @user = User.find(params[user.id])
+  def set_friend
+    @friend = Friend.find(params[:friend_id])
   end
 
   def booking_params
-    params.require(:booking).require(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
