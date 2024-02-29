552a2c2c0061262dac5a4b67ecf42140ced7fc87
bastien dubray <bastien.dubray@gmail.com>
2024-02-29 11:13:35 +0100

    Basic Flatpickr

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -6,38 +6,8 @@ export default class extends Controller {
   static targets =["openingDate", "endingDate"]
 
   connect() {
-    console.log("coucou")
-    console.log(this.openingDateTarget)
-    console.log(this.endingDateTarget)
     flatpickr(this.openingDateTarget,
-      {
-        dateFormat: "Y-m-d",
-        disable: [
-            {
-                from: "2024-02-01",
-                to: "2024-02-03"
-            },
-            {
-              from: "2024-02-10",
-              to: "2024-02-12"
-          }
-        ]
-      }
-    ),
-      flatpickr(this.endingDateTarget,
-      {
-        dateFormat: "Y-m-d",
-        disable: [
-            {
-                from: "2024-02-01",
-                to: "2024-02-03"
-            },
-            {
-              from: "2024-02-10",
-              to: "2024-02-12"
-          }
-        ]
-      }
+    this.endingDateTarget
     )
   }
 }
