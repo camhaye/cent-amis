21b465f4b86fa812e20ff133cbd85fceae9826dc
Camille <145802391+camhaye@users.noreply.github.com>
2024-03-02 21:58:22 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,21 @@ user1 = User.create!(pseudonyme: "Admin",
   password: 'password',
   password_confirmation: 'password',
   image_url: "https://i.imgur.com/WdjuE7y.jpeg")
-user2 = User.create!(pseudonyme: "toto-du-33",
+user2 = User.create!(pseudonyme: "Thomas",
   email: "thomas@mail.fr",
   password: 'password',
   password_confirmation: 'password',
   image_url: "https://i.imgur.com/6VpQxPy.jpeg")
-user3 = User.create!(pseudonyme: "bast1",
+user3 = User.create!(pseudonyme: "Bastien",
    email: "bastien@mail.fr",
    password: 'password',
    password_confirmation: 'password',
    image_url: "https://i.imgur.com/Mr2E09v.png")
-user4 = User.create!(pseudonyme: "aceillm",
+user4 = User.create!(pseudonyme: "Camille",
   email: "camille@mail.fr",
   password: 'password',
   password_confirmation: 'password',
-  image_url: "https://i.imgur.com/NaQmXdS.png")
+  image_url: "https://i.imgur.com/7VtAuwV.jpeg")
 
 puts 'User generated'
 
@@ -43,7 +43,7 @@ puts 'Generate Friends'
  friend1 = Friend.create!(first_name: "Romain",
   city: "Bordeaux",
   location: "Place de la Comédie, 33000 Bordeaux",
-  content: "Il aime la compétition, coder et jouer aux jeux vidés. Il apporte la fraicheur et les blagues douteuses.",
+  content: "Il aime la compétition, coder et jouer aux jeux vidéos. Il apporte la fraicheur et les blagues douteuses.",
   available: true,
   gender: 'Homme cis',
   age: 29,
@@ -67,7 +67,7 @@ puts 'Generate Friends'
   friend3 = Friend.create!(first_name: "Anne-Cécile",
   city: "Talence",
   location: "48 Avenue de la République, 33240 Saint-André-de-Cubzac",
-  content: "Elle aime et pratique la musique, boire des bières et régler les soucis de code. Elle apporte la sagesse et la communication.",
+  content: "Elle aime et pratique la musique, boire des bières et régler les galères de code. Elle apporte la sagesse et la communication.",
   available: true,
   gender: 'Femme cis',
   age: 35,
@@ -79,11 +79,11 @@ puts 'Generate Friends'
   friend4 = Friend.create!(first_name: "Benoît",
     city: "Bergerac",
     location: "19 Rue Neuve d'Argenson, 24100 Bergerac",
-    content: "Il aime la voile, l'aviation, les sports mécaniques (vroum vroum), les kebabs et faire le hater sur X. Il apporte la mauvaise humeur, des clopes de temps et temps et de la drogue.",
+    content: "Il aime la voile, l'aviation, les sports mécaniques (vroum vroum), les kebabs et faire le hater sur X. Il apporte la mauvaise humeur, des clopes de temps et temps et du CBD.",
     available: true,
     gender: 'Homme cis',
     age: 35,
-    good_at: "Pour écrire des avis google assassins",
+    good_at: "Écrire des avis google assassins",
     price: 20,
     image_url: "https://i.imgur.com/hnRWoG7.jpeg",
     user: user2)
